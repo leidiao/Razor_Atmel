3418f4815a6b53c8d8a505058b69f27fd0254253
leidiao <2481744220@qq.com>
2016-06-07 23:15:33 +0800

    procedure in PDF

--- firmware_mpg_common/application/user_app.h
+++ firmware_mpg_common/application/user_app.h
@@ -20,6 +20,8 @@ Header file for yournewtaskname.c
 #ifndef __USER_APP_H
 #define __USER_APP_H
 
+
+
 /**********************************************************************************************************************
 Type Definitions
 **********************************************************************************************************************/
@@ -28,7 +30,19 @@ Type Definitions
 /**********************************************************************************************************************
 Constants / Definitions
 **********************************************************************************************************************/
+/* Required constants for ANT channel configuration */
+#define ANT_CHANNEL_USERAPP             (u8)0                 
+#define ANT_SERIAL_LO_USERAPP           (u8)0              
+#define ANT_SERIAL_HI_USERAPP           (u8)0                 
+#define ANT_DEVICE_TYPE_USERAPP         (u8)0                 
+#define ANT_TRANSMISSION_TYPE_USERAPP   (u8)0                 
+#define ANT_CHANNEL_PERIOD_LO_USERAPP   (u8)0x00              
+#define ANT_CHANNEL_PERIOD_HI_USERAPP   (u8)0x20              
+#define ANT_FREQUENCY_USERAPP           (u8)50                
+#define ANT_TX_POWER_USERAPP            RADIO_TX_POWER_0DBM
 
+
+#define  TIMEOUT_VALUE                   (u32)3000   /*Time in ms  for state timeout*/
 
 /**********************************************************************************************************************
 Function Declarations
@@ -55,7 +69,9 @@ void UserAppRunActiveState(void);
 State Machine Declarations
 ***********************************************************************************************************************/
 static void UserAppSM_Idle(void);    
-
+static void  UserAppSM_WaitChannelOpen(void);
+static void UserAppSM_ChannelOpen(void) ;
+static void UserAppSM_WaitChannelClose(void);
 static void UserAppSM_Error(void);         
 static void UserAppSM_FailedInit(void);        
 
